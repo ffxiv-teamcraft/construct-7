6641e2205f32eaef8129b21ece1d479e44a888cc
Flavien Normand <contact@flavien-normand.fr>
2022-06-11 22:37:52 +0200

    initial commit

---
+++ apps/construct-7/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'constructmon-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less'],
+})
+export class AppComponent {
+  title = 'construct-7';
+}
