3342964137984e985c0faee0073bbf99d8ed9a25
Flavien Normand <contact@flavien-normand.fr>
2022-11-23 20:41:18 +0100

    chore: better grid system

--- apps/construct-7-electron/src/app/constants.ts
+++ apps/construct-7-electron/src/app/constants.ts
@@ -1,4 +1,4 @@
-export const rendererAppPort = 4200;
+export const rendererAppPort = 4201;
 export const rendererAppName = 'construct-7';
 export const electronAppName = 'construct-7-electron';
 export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
